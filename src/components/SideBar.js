39b17fa4bbbc2c5fca3e6c7cb757f9c946574c50
Wxmnath <Wxmnath@aol.com>
2022-04-19 21:17:49 +0100

    Search bar added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import qs from "qs";
 import "../styles/SideBar.css";
 
-const buildQueryString = (operation, valueObj) => {
-  const { search } = useLocation();
-
-  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
-  const newQueryParams = {
-    ...currentQueryParams,
-    [operation]: JSON.stringify(valueObj),
-  };
-  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
-};
 function SideBar() {
+  const [query, setQuery] = useState("");
+  const { search } = useLocation();
+  const history = useHistory();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    history.push(newQueryString);
+  };
+
   return (
     <div className="sidebar">
       <h5 className="sidebar-title">Filter By City:</h5>
-
       <Link
         className="link-item"
         to={buildQueryString("query", { city: "Manchester" })}
@@ -49,6 +67,18 @@ function SideBar() {
       <Link className="link-item" to={buildQueryString("sort", { price: +1 })}>
         Price Ascending
       </Link>
+      <form className="search-by-title" onSubmit={handleSearch}>
+        <button className="search-by-title-button" type="submit">
+          Search
+        </button>
+        <input
+          className="search-by-title-input"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search Property"
+        />
+      </form>
     </div>
   );
 }
